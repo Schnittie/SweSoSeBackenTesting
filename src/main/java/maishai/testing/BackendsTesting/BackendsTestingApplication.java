1d3fb64c96180dea31851957ffa52fa8d1768f5c
laurenz <i22032@hb.dhbw-stuttgart.de>
2024-03-17 21:08:04 +0100

    initial commit

---
+++ src/main/java/maishai/testing/BackendsTesting/BackendsTestingApplication.java
@@ -0,0 +1,13 @@
+package maishai.testing.BackendsTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maishai/testing/BackendsTesting/BackendsTestingApplicationTests.java
@@ -0,0 +1,18 @@
+package maishai.testing.BackendsTesting;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsTestingApplicationTests {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
